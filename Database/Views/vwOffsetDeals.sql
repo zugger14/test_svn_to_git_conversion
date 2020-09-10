546298ec8829ab8d0050761942a96d1ad558e24a
bmanandhar <bmanandhar>
2020-09-10 12:29:15 +0000

    Changed view to show transfer and offset deal from isnull(external_deal_id, close_referece_id). [Issue: 37401]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@111941 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/Views/vwOffsetDeals.sql
+++ Database/Views/vwOffsetDeals.sql
@@ -31,24 +31,25 @@ SELECT sdh.source_deal_header_id,
 	   ssbm.book_deal_type_map_id [sub_book_id]
 FROM source_deal_header sdh
 LEFT JOIN source_deal_header sdh_offset ON sdh_offset.close_reference_id = sdh.source_deal_header_id AND sdh_offset.deal_reference_type_id = 12500 
+LEFT JOIN source_deal_header sdh_offset_e ON sdh_offset_e.ext_deal_id = sdh.deal_id AND sdh_offset_e.deal_reference_type_id = 12500 
 LEFT JOIN source_system_book_map ssbm
-	ON ssbm.source_system_book_id1 = sdh_offset.source_system_book_id1
-	AND ssbm.source_system_book_id2 = sdh_offset.source_system_book_id2
-	AND ssbm.source_system_book_id3 = sdh_offset.source_system_book_id3
-	AND ssbm.source_system_book_id4 = sdh_offset.source_system_book_id4
+	ON ssbm.source_system_book_id1  = ISNULL(sdh_offset_e.source_system_book_id1, sdh_offset.source_system_book_id1)
+	AND ssbm.source_system_book_id2 = ISNULL(sdh_offset_e.source_system_book_id2, sdh_offset.source_system_book_id2)
+	AND ssbm.source_system_book_id3 = ISNULL(sdh_offset_e.source_system_book_id3, sdh_offset.source_system_book_id3)
+	AND ssbm.source_system_book_id4 = ISNULL(sdh_offset_e.source_system_book_id4, sdh_offset.source_system_book_id4)
 OUTER APPLY (
 	SELECT SUM(total_volume) total_volume, MAX(volume_multiplier2) volume_multiplier2, MAX(deal_volume)*MAX(volume_multiplier2) deal_volume, MAX(deal_volume) vol_only
 	FROM source_deal_detail sdd_offset 
-	WHERE sdd_offset.source_deal_header_id = sdh_offset.source_deal_header_id
+	WHERE sdd_offset.source_deal_header_id = ISNULL(sdh_offset_e.source_deal_header_id, sdh_offset.source_deal_header_id)
 ) sdd_offset
 OUTER APPLY (
 	SELECT MAX(deal_volume) deal_volume
 	FROM source_deal_detail sdd 
 	WHERE sdd.source_deal_header_id = sdh.source_deal_header_id
 ) sdd_original
-INNER JOIN source_counterparty sc ON sc.source_counterparty_id = sdh_offset.counterparty_id
-INNER JOIN source_traders st ON st.source_trader_id = sdh_offset.trader_id
-LEFT JOIN contract_group cg ON cg.contract_id = sdh_offset.contract_id
-WHERE sdh.source_deal_header_id <> sdh_offset.source_deal_header_id
-AND sdh_offset.source_deal_header_id IS NOT NULL
-AND sdh_offset.source_deal_header_id <> sdh.source_deal_header_id
+INNER JOIN source_counterparty sc ON sc.source_counterparty_id = ISNULL(sdh_offset_e.counterparty_id, sdh_offset.counterparty_id)
+INNER JOIN source_traders st ON st.source_trader_id = ISNULL(sdh_offset_e.trader_id, sdh_offset.trader_id)
+LEFT JOIN contract_group cg ON cg.contract_id = ISNULL(sdh_offset_e.contract_id, sdh_offset.contract_id)
+WHERE sdh.source_deal_header_id <> ISNULL(sdh_offset_e.source_deal_header_id, sdh_offset.source_deal_header_id)
+AND ISNULL(sdh_offset_e.source_deal_header_id,sdh_offset.source_deal_header_id) IS NOT NULL
+AND ISNULL(sdh_offset_e.source_deal_header_id, sdh_offset.source_deal_header_id) <> sdh.source_deal_header_id
--- Database/Views/vwTransferredDeals.sql
+++ Database/Views/vwTransferredDeals.sql
@@ -31,25 +31,27 @@ SELECT sdh.source_deal_header_id,
 	   ssbm.book_deal_type_map_id [sub_book_id]
 FROM source_deal_header sdh
 LEFT JOIN source_deal_header sdh_offset ON sdh_offset.close_reference_id = sdh.source_deal_header_id AND sdh_offset.deal_reference_type_id = 12500 
+LEFT JOIN source_deal_header sdh_offset_e ON sdh_offset_e.ext_deal_id = sdh.deal_id AND sdh_offset_e.deal_reference_type_id = 12500 
 LEFT JOIN source_deal_header sdh_transfer ON sdh_transfer.close_reference_id = ISNULL(sdh_offset.source_deal_header_id, sdh.source_deal_header_id) AND sdh_transfer.deal_reference_type_id = 12503
+LEFT JOIN source_deal_header sdh_transfer_e ON sdh_transfer_e.close_reference_id = ISNULL(sdh_offset_e.close_reference_id, sdh.source_deal_header_id) AND sdh_transfer_e.deal_reference_type_id = 12503
 LEFT JOIN source_system_book_map ssbm
-	ON ssbm.source_system_book_id1 = sdh_transfer.source_system_book_id1
-	AND ssbm.source_system_book_id2 = sdh_transfer.source_system_book_id2
-	AND ssbm.source_system_book_id3 = sdh_transfer.source_system_book_id3
-	AND ssbm.source_system_book_id4 = sdh_transfer.source_system_book_id4
+	ON ssbm.source_system_book_id1  = ISNULL(sdh_transfer_e.source_system_book_id1, sdh_transfer.source_system_book_id1)
+	AND ssbm.source_system_book_id2 = ISNULL(sdh_transfer_e.source_system_book_id2, sdh_transfer.source_system_book_id2)
+	AND ssbm.source_system_book_id3 = ISNULL(sdh_transfer_e.source_system_book_id3, sdh_transfer.source_system_book_id3)
+	AND ssbm.source_system_book_id4 = ISNULL(sdh_transfer_e.source_system_book_id4, sdh_transfer.source_system_book_id4)
 OUTER APPLY (
 	SELECT SUM(total_volume) total_volume, MAX(volume_multiplier2) volume_multiplier2, MAX(deal_volume)*MAX(volume_multiplier2) deal_volume, MAX(deal_volume) vol_only
 	FROM source_deal_detail sdd_transfer 
-	WHERE sdd_transfer.source_deal_header_id = sdh_transfer.source_deal_header_id
+	WHERE sdd_transfer.source_deal_header_id = ISNULL(sdh_transfer_e.source_deal_header_id, sdh_transfer.source_deal_header_id)
 ) sdd_transfer
 OUTER APPLY (
 	SELECT MAX(deal_volume) deal_volume
 	FROM source_deal_detail sdd 
 	WHERE sdd.source_deal_header_id = sdh.source_deal_header_id
 ) sdd_original
-INNER JOIN source_counterparty sc ON sc.source_counterparty_id = sdh_transfer.counterparty_id
-INNER JOIN source_traders st ON st.source_trader_id = sdh_transfer.trader_id
-LEFT JOIN contract_group cg ON cg.contract_id = sdh_transfer.contract_id
-WHERE sdh.source_deal_header_id <> sdh_transfer.source_deal_header_id
-AND sdh_transfer.source_deal_header_id IS NOT NULL
-AND sdh_offset.source_deal_header_id <> sdh.source_deal_header_id
+INNER JOIN source_counterparty sc ON sc.source_counterparty_id = ISNULL(sdh_transfer_e.counterparty_id, sdh_transfer.counterparty_id)
+INNER JOIN source_traders st ON st.source_trader_id = ISNULL(sdh_transfer_e.trader_id, sdh_transfer.trader_id)
+LEFT JOIN contract_group cg ON cg.contract_id = ISNULL(sdh_transfer_e.contract_id, sdh_transfer.contract_id)
+WHERE ISNULL(sdh.source_deal_header_id, 1) <> coalesce(sdh_transfer_e.source_deal_header_id, sdh_transfer.source_deal_header_id, 1)
+AND ISNULL(sdh_transfer_e.source_deal_header_id, sdh_transfer.source_deal_header_id) IS NOT NULL
+AND ISNULl(sdh_offset_e.source_deal_header_id, sdh_offset.source_deal_header_id) <> sdh.source_deal_header_id
