1ef761906d60d391ad54cb709ca67ebea79aba0f
rabhusal <rabhusal>
2020-09-17 12:11:10 +0000

    Changed logic to skip only Step 1, 2 and 3. The rest are required. [Issue: 37644]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@112147 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/StoredProc/spa_source_deal_header.sql
+++ Database/StoredProc/spa_source_deal_header.sql
@@ -2103,14 +2103,8 @@ BEGIN
 		EXEC(@sql)
 
 		EXEC spa_register_event 20601, 10000322, @alert_process_table, 0, @deal_lock_process_id
-
-		EXEC spa_insert_update_audit 'u', @audit_ids
 		
-		/*
-			Commented below part because, locking/unlocking deal expects the deal to get locked/unlocked and the audit part needs to be done.
-			Re-enable if needed for new case.
-		*/
-		/* SET @after_update_process_table = dbo.FNAProcessTableName('after_insert_process_table', @user_name, @job_process_id)
+		SET @after_update_process_table = dbo.FNAProcessTableName('after_insert_process_table', @user_name, @job_process_id)
 		
 		IF OBJECT_ID(@after_update_process_table) IS NOT NULL
 		BEGIN
@@ -2124,10 +2118,10 @@ BEGIN
 					FROM dbo.SplitCommaSeperatedValues(''' + @deal_ids + ''')'
 		EXEC(@sql)
 			
-		SET @sql = 'spa_deal_insert_update_jobs ''u'', ''' + @after_update_process_table + ''''
+		SET @sql = 'spa_deal_insert_update_jobs ''u'', ''' + @after_update_process_table + ''', ''1,2,3'''
 		SET @job_name = 'spa_deal_insert_update_jobs_' + @job_process_id
  		
-		EXEC spa_run_sp_as_job @job_name, @sql, 'spa_deal_insert_update_jobs', @user_name */
+		EXEC spa_run_sp_as_job @job_name, @sql, 'spa_deal_insert_update_jobs', @user_name
 
 		SET @desc = 'Deal(s) successfully ' + CASE WHEN @lock_unlock = 'y' THEN ' locked. ' ELSE ' unlocked.' END		
 		EXEC spa_ErrorHandler 0
