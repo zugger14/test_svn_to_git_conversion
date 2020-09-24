4e94931911c4fe16b2123d6ca5d77f89f81a4eaa
dmanandhar <dmanandhar>
2020-09-24 11:24:36 +0000

    Added new run process to transfer position of a book and location to another book with same location. [Issue: 37659]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@112379 fd52c265-6bb1-e54c-ad91-b501145a01df

---
+++ Database/ReportManager/Reports/Position Formula.sql
@@ -0,0 +1,47 @@
+BEGIN TRY
+		BEGIN TRAN
+
+		DECLARE @report_id_dest INT 
+	
+NULL
+
+		declare @report_copy_name varchar(200)
+		
+		set @report_copy_name = isnull(@report_copy_name, 'Copy of ' + 'position formulation')
+		
+
+		--RETAIN APPLICATION FILTER DETAILS START (PART2)
+		update pm
+		set inserted_paramset_id = rp.report_paramset_id
+		from #paramset_map pm
+		inner join report_paramset rp on rp.paramset_hash = pm.paramset_hash
+		
+		update f set f.report_id = pm.inserted_paramset_id
+		from application_ui_filter f
+		inner join #paramset_map pm on pm.deleted_paramset_id = isnull(f.report_id, -1)
+		where f.application_function_id is null
+	
+		delete fd
+		--select *
+		from application_ui_filter_details fd
+		inner join application_ui_filter f on f.application_ui_filter_id = fd.application_ui_filter_id
+		inner join #paramset_map pm on pm.inserted_paramset_id = isnull(f.report_id, -1)
+		where abs(fd.report_column_id) not in (
+			select distinct rp.column_id
+			from report_param rp
+			inner join report_dataset_paramset rdp on rdp.report_dataset_paramset_id = rp.dataset_paramset_id
+			inner join report_paramset rpm on rpm.report_paramset_id = rdp.paramset_id
+			where rpm.report_paramset_id = f.report_id
+		)
+		--RETAIN APPLICATION FILTER DETAILS END (PART2)
+	
+COMMIT 
+
+END TRY
+BEGIN CATCH
+	IF @@TRANCOUNT > 0
+		ROLLBACK TRAN;
+		
+	DECLARE @error_message VARCHAR(MAX) = ERROR_MESSAGE()
+	RAISERROR(@error_message,16,1)
+END CATCH
