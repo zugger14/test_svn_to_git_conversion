f0585150cfdaf59e822d85f1b857d69d6063d0b8
bmanandhar <bmanandhar>
2020-09-28 12:29:25 +0000

    Merged revision(s) 112387 from branches/TRMTracker_Release/Ver_4.2.100: Alter table script. ........
    [Issue: 37833]
    
    
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@112512 fd52c265-6bb1-e54c-ad91-b501145a01df

---
+++ Database/TableChanges/25660_alter_table_master_deal_view_counterparty_contract_location.sql
@@ -0,0 +1,29 @@
+IF COL_LENGTH('master_deal_view', 'counterparty') IS NOT NULL
+BEGIN
+    ALTER TABLE 
+	/**
+        Columns
+        name : Counterparty Name
+    */
+	master_deal_view ALTER COLUMN counterparty VARCHAR(500) NOT NULL
+END
+
+IF COL_LENGTH('master_deal_view', 'contract') IS NOT NULL
+BEGIN
+    ALTER TABLE 
+	/**
+        Columns
+        name : Contract Name
+    */
+	master_deal_view ALTER COLUMN contract VARCHAR(500)
+END
+
+IF COL_LENGTH('master_deal_view', 'location') IS NOT NULL
+BEGIN
+    ALTER TABLE
+	/**
+        Columns
+        name : Location Name
+    */
+	master_deal_view ALTER COLUMN location VARCHAR(500)
+END
