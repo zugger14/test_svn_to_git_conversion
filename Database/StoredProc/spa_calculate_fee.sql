d263a73a960da942fec2a8a5ef7d223623e71773
arai <arai>
2020-10-15 19:34:59 +0000

    - Corrected process table name. [Issue: 38146]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@113145 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/StoredProc/spa_calculate_fee.sql
+++ Database/StoredProc/spa_calculate_fee.sql
@@ -205,7 +205,7 @@ CREATE TABLE #fees_breakdown_tax
 
 	----###### Evaluate formula defined in UDF
 	DECLARE  @formula_table5 VARCHAR(100),@calc_result_table5 VARCHAR(100),@calc_result_table_breakdown5 VARCHAR(100)
-	SET @formula_table5 = dbo.FNAProcessTableName('udf_formula', @user_id, @process_id)
+	SET @formula_table5 = dbo.FNAProcessTableName('udf_formula_tax', @user_id, @process_id)
 
 
 
