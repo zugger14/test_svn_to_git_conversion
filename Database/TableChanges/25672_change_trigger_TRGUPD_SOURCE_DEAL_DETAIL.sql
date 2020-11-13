1ab9735779d8f86490766f4710a19a9b7eba2e5b
rabhusal <rabhusal>
2020-11-13 10:51:48 +0000

    Removed comments. [Issue: 38392]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@113809 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/TableChanges/25672_change_trigger_TRGUPD_SOURCE_DEAL_DETAIL.sql
+++ Database/TableChanges/25672_change_trigger_TRGUPD_SOURCE_DEAL_DETAIL.sql
@@ -13,30 +13,13 @@ ON [dbo].[source_deal_detail]
 FOR UPDATE
 AS
 
---the table will be created in the sp spa_update_deal_total_volume, and it is not required to update timestamp
-IF OBJECT_ID('tempdb..#is_total_volume_only_update') IS NULL
-BEGIN 
-	DECLARE @datetime DATETIME = GETDATE()
+DECLARE @datetime DATETIME = GETDATE()
 
-    UPDATE source_deal_detail
-    SET update_user = dbo.FNADBUser(),
-        update_ts = @datetime
-    FROM [source_deal_detail] s
-	INNER JOIN deleted i ON s.source_deal_detail_id = i.source_deal_detail_id
-	-- (summy bidur) rigesh gyan sushil annal deal capture owners
-
-	/** Disabled this because, the timestamp of deal is updated from Stored Procedure and is handled in Import as well.
-		-- To update the update_ts of source_deal_header same as update_ts of source_deal_detail when a shaped hourly deal is imported.
-		UPDATE sdh
-		SET sdh.update_ts = @datetime
-		FROM source_deal_header sdh
-		INNER JOIN (
-			SELECT MAX(source_deal_header_id) source_deal_header_id
-			FROM INSERTED
-		) i ON sdh.source_deal_header_id = i.source_deal_header_id
-			AND ISNULL(sdh.internal_desk_id, 17300) = 17302
-	**/
-END
+UPDATE source_deal_detail
+SET update_user = dbo.FNADBUser(),
+    update_ts = @datetime
+FROM [source_deal_detail] s
+INNER JOIN deleted i ON s.source_deal_detail_id = i.source_deal_detail_id
 
 IF UPDATE(deal_volume)
 BEGIN
@@ -48,5 +31,5 @@ END
 
 GO
 
-ALTER TABLE [dbo].[source_deal_detail] ENABLE TRIGGER [TRGUPD_SOURCE_DEAL_DETAIL]
+ALTER TABLE [dbo].[source_deal_detail] /** */ ENABLE TRIGGER [TRGUPD_SOURCE_DEAL_DETAIL]
 GO
