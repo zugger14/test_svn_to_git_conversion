ffd6b0fde9ea7a3426ab71b0c2312252252c8ae6
gkoju <gkoju>
2020-11-20 08:53:35 +0000

    SP Base position report enhancement [Issue: 37612]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@113912 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/TableChanges/25676_add_source_minor_location_station_id.sql
+++ Database/TableChanges/25676_add_source_minor_location_station_id.sql
@@ -1,2 +1,9 @@
-alter table source_minor_location add station_id int
-alter table source_minor_location add dam_id int
+
+IF NOT EXISTS(SELECT 1 FROM sys.tables  t INNER JOIN sys.columns c ON c.object_id = t.object_id where t.name = 'source_minor_location' AND c.name = 'station_id')
+BEGIN
+	alter table source_minor_location add station_id int
+	alter table source_minor_location add dam_id int
+END
+ELSE 
+	PRINT 'Column already added'
+
