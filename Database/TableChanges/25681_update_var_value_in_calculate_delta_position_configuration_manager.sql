28f19f27768b6dfa1bde0df798cfe42c33f70b55
sona.maharjan <sona.maharjan>
2020-11-27 12:05:18 +0000

    1.Change the default value of var_value in calculate detla position 2.Truncate the tables related to detla position [Issue: 38468]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@114114 fd52c265-6bb1-e54c-ad91-b501145a01df

---
+++ Database/TableChanges/25681_update_var_value_in_calculate_delta_position_configuration_manager.sql
@@ -0,0 +1,3 @@
+UPDATE adiha_default_codes_values
+SET var_value=0
+WHERE default_code_id = 103
---
+++ Database/TableChanges/25682_truncate_tables_related_to_delta_position.sql
@@ -0,0 +1,10 @@
+/** main table of view: delta_report_hourly_position */
+TRUNCATE table delta_report_hourly_position_main 
+
+/** main table of view: delta_report_hourly_position_breakdown*/
+TRUNCATE table delta_report_hourly_position_breakdown_main
+
+/** main table of view:delta_report_hourly_position_financial */
+TRUNCATE table delta_report_hourly_position_financial_main
+
+TRUNCATE table delta_report_hourly_position_profile
