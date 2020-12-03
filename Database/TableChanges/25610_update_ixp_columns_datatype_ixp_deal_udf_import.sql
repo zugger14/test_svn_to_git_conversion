fabe89a638c814daef82b8e98e0fabc54bbf1293
bkoirala <bkoirala>
2020-12-03 10:44:07 +0000

    Updated data type to null. [Issue: 38532]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@114252 fd52c265-6bb1-e54c-ad91-b501145a01df

---
+++ Database/TableChanges/25610_update_ixp_columns_datatype_ixp_deal_udf_import.sql
@@ -0,0 +1,8 @@
+DECLARE @ixp_tables_id INT 
+SELECT @ixp_tables_id =  ixp_tables_id FROM ixp_tables WHERE ixp_tables_name = 'ixp_deal_udf_import'
+
+UPDATE
+ixp_columns
+SET datatype = NULL
+WHERE ixp_table_id = @ixp_tables_id
+AND ixp_columns_name = 'value'
