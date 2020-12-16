3262977e7381007112d455ef4bef489b0032a987
spneupane <spneupane>
2020-12-16 04:05:36 +0000

    Added enable queue option for  jobs created by XConnect. [Issue: 38652]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@114561 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/StoredProc/spa_xconnect.sql
+++ Database/StoredProc/spa_xconnect.sql
@@ -129,7 +129,7 @@ BEGIN
 			WHERE icd.interface_name = @exchange_name
 		END
 				
-		SET @sql = ' spa_ixp_rules @flag = ''t'',@process_id = ''' + @process_id + ''',@ixp_rules_id =' + ISNULL(CAST(@ixp_rules_id AS VARCHAR), '0') + ',@run_table = ''' + @process_table_name + ''',@source = ''21400'',@run_with_custom_enable = ''n'',@run_in_debug_mode=''y'''
+		SET @sql = ' spa_ixp_rules @flag = ''t'',@process_id = ''' + @process_id + ''',@ixp_rules_id =' + ISNULL(CAST(@ixp_rules_id AS VARCHAR), '0') + ',@run_table = ''' + @process_table_name + ''',@source = ''21400'',@run_with_custom_enable = ''n'',@run_in_debug_mode=''y'', @execute_in_queue=1'
 		--print @sql
 		SET @job_name = 'Import_' + REPLACE(@exchange_name, ' ', '_') + '_' +ISNULL(CAST(@ixp_rules_id AS VARCHAR), '0') + '_' + @process_id;
 		DECLARE @user_login_id VARCHAR(255) = dbo.FNAAppAdminID()
