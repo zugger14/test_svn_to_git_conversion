441286e1182e91eb747a698298a4ae76e1c9a253
msingh <msingh>
2021-02-04 11:26:25 +0000

    Fixed incorrect event triggering sequence. [Issue: 39072]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@115660 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/StoredProc/spa_calendar.sql
+++ Database/StoredProc/spa_calendar.sql
@@ -735,9 +735,10 @@ BEGIN
 
 	WHILE @@FETCH_STATUS = 0   
 	BEGIN  
-		SELECT DISTINCT TOP(1) @alert_id = ett.alert_id, @event_trigger_id = ett.event_trigger_id
+		SELECT TOP(1) @alert_id = ett.alert_id, @event_trigger_id = ett.event_trigger_id
 		FROM module_events mee
-		LEFT JOIN event_trigger ett ON mee.module_events_id = ett.modules_event_id
+		INNER JOIN event_trigger ett ON mee.module_events_id = ett.modules_event_id
+		INNER JOIN workflow_schedule_task wst ON wst.workflow_id = ett.event_trigger_id
 		LEFT JOIN (	
 			SELECT DISTINCT wea.alert_id
 			FROM module_events me
@@ -749,6 +750,7 @@ BEGIN
 		) a ON a.alert_id = ett.event_trigger_id
 		LEFT JOIN alert_sql as2 ON ett.alert_id = as2.alert_sql_id
 		WHERE ett.modules_event_id = @workflow_id AND (a.alert_id IS NULL OR ett.initial_event = 'y') and as2.is_active = 'y'
+		ORDER BY wst.sort_order
 
 		SET @process_id = dbo.FNAGetNewID();
 		SET @alert_sql_statement = 'spa_run_alert_sql ' + CAST(@alert_id AS VARCHAR) + ', NULL, NULL, NULL, NULL, ' + CAST(@event_trigger_id AS VARCHAR) + ',NULL,NULL,NULL,' + CAST(@w_group_id AS VARCHAR)
