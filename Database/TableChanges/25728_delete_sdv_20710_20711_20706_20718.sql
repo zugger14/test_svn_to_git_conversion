2ec658d7210156c45af7db495af3667395103455
bmaharjan <bmaharjan>
2021-02-11 15:38:34 +0000

    Removed the unrequired code. [Issue: 38978]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@115862 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/TableChanges/25728_delete_sdv_20710_20711_20706_20718.sql
+++ Database/TableChanges/25728_delete_sdv_20710_20711_20706_20718.sql
@@ -1,8 +1,8 @@
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME = 'fk_deal_actualization_detail_actualization_status_static_data_value')
-BEGIN
-	ALTER TABLE deal_actualization_detail
-	DROP CONSTRAINT fk_deal_actualization_detail_actualization_status_static_data_value
-END
+--IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME = 'fk_deal_actualization_detail_actualization_status_static_data_value')
+--BEGIN
+--	ALTER TABLE deal_actualization_detail
+--	DROP CONSTRAINT fk_deal_actualization_detail_actualization_status_static_data_value
+--END
 
 -- Delete Unactualized static data value
 IF EXISTS(SELECT * FROM static_data_value WHERE type_id = 20700 AND value_id IN (20706,20718,20710,20711))
@@ -10,10 +10,10 @@ BEGIN
 	DELETE FROM static_data_value WHERE type_id = 20700 AND value_id IN (20706,20718,20710,20711)
 END
 
-IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME = 'fk_deal_actualization_detail_actualization_status_static_data_value')
-BEGIN
-	ALTER TABLE deal_actualization_detail
-	ADD CONSTRAINT fk_deal_actualization_detail_actualization_status_static_data_value FOREIGN KEY ([actualization_status]) REFERENCES static_data_value([value_id])
-END
-GO
+--IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME = 'fk_deal_actualization_detail_actualization_status_static_data_value')
+--BEGIN
+--	ALTER TABLE deal_actualization_detail
+--	ADD CONSTRAINT fk_deal_actualization_detail_actualization_status_static_data_value FOREIGN KEY ([actualization_status]) REFERENCES static_data_value([value_id])
+--END
+--GO
 
