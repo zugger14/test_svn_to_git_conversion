4dd7ecfc972288491b18a02ac24af6afd223212d
msingh <msingh>
2021-02-15 06:51:12 +0000

    Fixed FK issue raised by delete statement. [Issue: 38962]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@115913 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/TableChanges/25729_delete_application_functions_application_functional_users_10105803.sql
+++ Database/TableChanges/25729_delete_application_functions_application_functional_users_10105803.sql
@@ -1,2 +1,7 @@
-DELETE FROM application_functions WHERE function_id = 10105803
-DELETE FROM application_functional_users WHERE function_id = 10105803
+--Remove privilege from roles and users
+DELETE FROM application_functional_users 
+WHERE function_id = 10105803
+
+--Delete function id
+DELETE FROM application_functions 
+WHERE function_id = 10105803
