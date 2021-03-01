376c9ddb35c2d3116a6d47cd62a7da982d145534
gkoju <gkoju>
2021-03-01 12:16:44 +0000

    Add criteria to filter missing position selection. [Issue: 39269]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@116224 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/StoredProc/spa_calc_missing_hourly_position.sql
+++ Database/StoredProc/spa_calc_missing_hourly_position.sql
@@ -1,85 +1,99 @@
---IF  EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID(N'[dbo].[spa_calc_missing_hourly_position]') AND TYPE IN (N'P', N'PC'))
---DROP PROCEDURE [dbo].[spa_calc_missing_hourly_position]
-	
---SET ANSI_NULLS ON
---GO
---SET QUOTED_IDENTIFIER ON
---GO
---create proc [dbo].[spa_calc_missing_hourly_position] @sub_id int=null
---as
+IF EXISTS (SELECT * FROM sys.objects WHERE OBJECT_ID = OBJECT_ID(N'[dbo].[spa_calc_missing_hourly_position]') AND TYPE IN (N'P', N'PC'))
+DROP PROCEDURE [dbo].[spa_calc_missing_hourly_position]
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create proc [dbo].[spa_calc_missing_hourly_position] @sub_id int=null
+as
 
---/*
+/*
 
 if object_id('tempdb..#books') is not null
 drop table #books
 
 declare @sub_id int
 
-
---select * from process_deal_position_breakdown 
+--select * from process_deal_position_breakdown
 --delete process_deal_position_breakdown
 
-
 --*/
-
 
 declare @report_position varchar(250),@process_id varchar(50),@st varchar(max),@db_usr varchar(30)
 set @db_usr= dbo.FNADBUser()
 set @process_id=dbo.FNAGetNewID()
 SET @report_position = dbo.FNAProcessTableName('report_position', @db_usr, @process_id)
 
-
 --print('create table '+@report_position+' (source_deal_header_id int, [action] varchar(1))')
 --exec('create table '+@report_position+' (source_deal_header_id int, [action] varchar(1))')
 
 -- Taking deals that mapped to imported profile through location.
 
-CREATE TABLE #books (fas_book_id INT, source_system_book_id1 INT,source_system_book_id2 INT,source_system_book_id3 INT,source_system_book_id4 INT) 
+CREATE TABLE #books (fas_book_id INT, source_system_book_id1 INT,source_system_book_id2 INT,source_system_book_id3 INT,source_system_book_id4 INT)
 
 set @st='
-	INSERT INTO  #books
-	SELECT  distinct 
-	ssbm.fas_book_id,ssbm.source_system_book_id1,ssbm.source_system_book_id2,ssbm.source_system_book_id3,ssbm.source_system_book_id4
-	FROM portfolio_hierarchy book (nolock) INNER JOIN
-	Portfolio_hierarchy stra (nolock) ON book.parent_entity_id = stra.entity_id LEFT OUTER JOIN            
-	source_system_book_map ssbm ON ssbm.fas_book_id = book.entity_id           
-	where 1=1' +case when @sub_id is null then '' else ' stra.parent_entity_id='+cast(@sub_id as varchar) end
+INSERT INTO #books
+SELECT distinct
+ssbm.fas_book_id,ssbm.source_system_book_id1,ssbm.source_system_book_id2,ssbm.source_system_book_id3,ssbm.source_system_book_id4
+FROM portfolio_hierarchy book (nolock) INNER JOIN
+Portfolio_hierarchy stra (nolock) ON book.parent_entity_id = stra.entity_id LEFT OUTER JOIN
+source_system_book_map ssbm ON ssbm.fas_book_id = book.entity_id
+where 1=1' +case when @sub_id is null then '' else ' stra.parent_entity_id='+cast(@sub_id as varchar) end
 
 exec(@st)
 
 set @st='
-	SELECT DISTINCT sdh.source_deal_header_id, ''i'' action
-	--INTO '+@report_position+'
-	FROM source_deal_header sdh (nolock) 
-	inner join #books ssbm on 
+SELECT DISTINCT sdh.source_deal_header_id, ''i'' action,sdd.source_deal_detail_id
+INTO '+@report_position+'
+FROM source_deal_header sdh (nolock)
+	inner join #books ssbm on
 		ssbm.source_system_book_id1=sdh.source_system_book_id1 and ssbm.source_system_book_id2=sdh.source_system_book_id2
 		and ssbm.source_system_book_id3=sdh.source_system_book_id3 and ssbm.source_system_book_id4=sdh.source_system_book_id4
+	inner join source_deal_header_template sdht on sdht.template_id=sdh.template_id
 	inner join source_deal_detail sdd on sdh.source_deal_header_id=sdd.source_deal_header_id
-	left join report_hourly_position_deal s on sdh.source_deal_header_id=s.source_deal_header_id
-	where (s.source_deal_header_id is null or sdd.total_volume is null)
-	and  isnull(sdh.internal_desk_id,17300) in (17300,17302) and  isnull(sdh.product_id,4101) = 4101
-	union
-	SELECT DISTINCT sdh.source_deal_header_id, ''i'' action
-	FROM source_deal_header sdh (nolock) 
-	inner join #books ssbm on 
+	left join report_hourly_position_deal_main s on s.term_start between sdd.term_start and sdd.term_end and   sdd.source_deal_detail_id=s.source_deal_detail_id
+	left join source_deal_detail_position sddp on sddp.source_deal_detail_id=sdd.source_deal_detail_id
+where (s.source_deal_header_id is null or sddp.total_volume is null)
+	and isnull(sdh.internal_desk_id,17300) in (17300,17302) and isnull(sdh.product_id,4101) = 4101
+	and sdd.fixed_float_leg = ''t'' AND ISNULL(sdht.internal_deal_type_value_id,-1) NOT IN(21,20)
+	and isnull(sdd.deal_volume,0)<>0
+union
+SELECT DISTINCT sdh.source_deal_header_id, ''i'' action,sdd.source_deal_detail_id
+FROM source_deal_header sdh
+	inner join #books ssbm on
 		ssbm.source_system_book_id1=sdh.source_system_book_id1 and ssbm.source_system_book_id2=sdh.source_system_book_id2
 		and ssbm.source_system_book_id3=sdh.source_system_book_id3 and ssbm.source_system_book_id4=sdh.source_system_book_id4
+	inner join source_deal_header_template sdht on sdht.template_id=sdh.template_id
 	inner join source_deal_detail sdd on sdh.source_deal_header_id=sdd.source_deal_header_id
-	left join report_hourly_position_profile s on sdh.source_deal_header_id=s.source_deal_header_id
-	where (s.source_deal_header_id is null or sdd.total_volume is null)
-	and  sdh.internal_desk_id in (17301) and  isnull(sdh.product_id,4101) = 4101
-	union
-	SELECT DISTINCT sdh.source_deal_header_id, ''i'' action
-	FROM source_deal_header sdh (nolock)
-	inner join #books ssbm on 
+	left join source_deal_detail_position sddp on sddp.source_deal_detail_id=sdd.source_deal_detail_id
+	left join report_hourly_position_profile_main s on s.term_start between sdd.term_start and sdd.term_end and sdd.source_deal_header_id=s.source_deal_header_id
+	outer apply
+	(
+	select 
+		sum(Hr1+Hr2+Hr3+Hr4+Hr5+Hr6+Hr7+Hr8+Hr9+Hr10+Hr11+Hr12+Hr13+Hr14+Hr15+Hr16+Hr17+Hr18+Hr19+Hr20+Hr21+Hr22+Hr23+Hr24) forecast
+		from deal_detail_hour where profile_id=sdd.profile_id and term_date between sdd.term_start and sdd.term_end
+	) forecast
+where (s.source_deal_header_id is null or sddp.total_volume is null)
+	and sdh.internal_desk_id in (17301) and isnull(sdh.product_id,4101) = 4101
+	and sdd.fixed_float_leg = ''t'' AND ISNULL(sdht.internal_deal_type_value_id,-1) NOT IN(21,20) 
+	and isnull(forecast.forecast,0)<>0
+union
+SELECT DISTINCT sdh.source_deal_header_id, ''i'' action,sdd.source_deal_detail_id
+FROM source_deal_header sdh
+	inner join #books ssbm on
 		ssbm.source_system_book_id1=sdh.source_system_book_id1 and ssbm.source_system_book_id2=sdh.source_system_book_id2
 		and ssbm.source_system_book_id3=sdh.source_system_book_id3 and ssbm.source_system_book_id4=sdh.source_system_book_id4
+	inner join source_deal_header_template sdht on sdht.template_id=sdh.template_id
 	inner join source_deal_detail sdd on sdh.source_deal_header_id=sdd.source_deal_header_id
-	left join report_hourly_position_fixed s on sdh.source_deal_header_id=s.source_deal_header_id
-	where (s.source_deal_header_id is null or sdd.total_volume is null)
-	and  sdh.product_id =4100'
+	left join source_deal_detail_position sddp on sddp.source_deal_detail_id=sdd.source_deal_detail_id
+	left join report_hourly_position_fixed_main s on s.term_start between sdd.term_start and sdd.term_end and sdd.source_deal_detail_id=s.source_deal_detail_id
+where (s.source_deal_header_id is null or sddp.total_volume is null)
+	and sdh.product_id =4100 and sdd.fixed_float_leg = ''t'' AND ISNULL(sdht.internal_deal_type_value_id,-1) NOT IN(21,20) 
+'
 
 print(@st)
 exec(@st)
---if @@ROWCOUNT>0
---	EXEC dbo.spa_update_deal_total_volume NULL, @process_id, 0,1,@db_usr,'n',2
+
+if @@ROWCOUNT>0
+	EXEC dbo.spa_update_deal_total_volume NULL, @process_id, 0,1,@db_usr,'n',2,default,'n'
+
