ba67c6b8909e39d525ff7f6c0134319a8ef26a44
sonam.sherpa <sonam.sherpa>
2021-03-30 05:49:02 +0000

    Handled cases for existing job descriptions [Issue: 39284]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@116750 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/Functions/FNAGetJobDescription.sql
+++ Database/Functions/FNAGetJobDescription.sql
@@ -25,21 +25,18 @@ RETURNS @items TABLE (
 )
 AS
 BEGIN
-    INSERT INTO @items(job_id, [description], [user_login_id])
+    INSERT INTO @items(job_id, [user_login_id], [description])
 	SELECT 
 		sj.job_id
+		,ISNULL(au.user_login_id,'') [user_name]
 		,CASE 
 			WHEN CHARINDEX(CHAR(13), sj.description,0) <> 0 THEN LTRIM(RTRIM(i.clm2)) 
-			ELSE sj.description 
+			ELSE sj.description
 		END [desciption]
-		,CASE 
-			WHEN CHARINDEX(CHAR(13), sj.description,0) <> 0 THEN REPLACE(LTRIM(RTRIM(i.clm1)),'Created by: ', '')
-			ELSE '' 
-		END [user_name]
 	FROM  msdb.dbo.sysjobs sj
 	OUTER APPLY (SELECT clm1,clm2 FROM dbo.FNASplitAndTranspose(sj.description, CHAR(13))) i
+	LEFT JOIN application_users au ON au.user_login_id = REPLACE(LTRIM(RTRIM(i.clm1)),'Created by: ', '')
 	WHERE sj.job_id = @job_id
-	
 	RETURN
 END
 GO
