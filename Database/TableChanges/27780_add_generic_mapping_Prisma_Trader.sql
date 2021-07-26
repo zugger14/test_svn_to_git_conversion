a8c8c702ac1cc847a58b434241a7908d3ac5f787
manish.aryal <manish.aryal>
2021-07-26 09:23:21 +0000

    Used existing udf and static data and updated name. [Issue: CTRM-2500]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@119152 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/TableChanges/27780_add_generic_mapping_Prisma_Trader.sql
+++ Database/TableChanges/27780_add_generic_mapping_Prisma_Trader.sql
@@ -1,41 +1,41 @@
-DELETE static_data_value WHERE [type_id] = 5500 AND code = 'Prisma Trader' AND value_id > 0
+--DELETE static_data_value WHERE [type_id] = 5500 AND code = 'Source Trader' AND value_id > 0
 
 SET IDENTITY_INSERT static_data_value ON
-IF NOT EXISTS(SELECT 1 FROM static_data_value WHERE value_id = -10000371)
+IF NOT EXISTS(SELECT 1 FROM static_data_value WHERE code = 'Source Trader')
 BEGIN
     INSERT INTO static_data_value ([type_id], [value_id], [code], [description], [category_id], create_user, create_ts)
-    VALUES (5500, -10000371, 'Prisma Trader', 'Prisma Trader', NULL, 'farrms_admin', GETDATE())
-    PRINT 'Inserted static data value -10000371 - Prisma Trader.'
+    VALUES (5500, 50000192, 'Source Trader', 'Source Trader', NULL, 'farrms_admin', GETDATE())
+    PRINT 'Inserted static data value 50000192 - Source Trader.'
 END
 ELSE
 BEGIN
-    PRINT 'Static data value -10000371 - Prisma Trader already EXISTS.'
+    PRINT 'Static data value 50000192 - Prisma Trader already EXISTS.'
 END
 SET IDENTITY_INSERT static_data_value OFF
 
-IF NOT EXISTS(SELECT 1 FROM user_defined_fields_template WHERE field_id = -10000371)
+IF NOT EXISTS(SELECT 1 FROM user_defined_fields_template WHERE field_id = 50000192)
 BEGIN
 	INSERT INTO user_defined_fields_template(field_name, field_label, Field_type, data_type, is_required, sql_string, udf_type, field_size, field_id)
-	SELECT -10000371, 'Prisma Trader', 't', 'VARCHAR(100)', 'y', NULL, 'h', 100, -10000371
+	SELECT 50000192, 'Source Trader', 't', 'VARCHAR(100)', 'y', NULL, 'h', 100, 50000192
 END
 
 SET IDENTITY_INSERT static_data_value ON
-IF NOT EXISTS(SELECT 1 FROM static_data_value WHERE value_id = -10000253)
+IF NOT EXISTS(SELECT 1 FROM static_data_value WHERE value_id = 300006)
 BEGIN
     INSERT INTO static_data_value ([type_id], [value_id], [code], [description], [category_id], create_user, create_ts)
-    VALUES (5500, -10000253, 'Trader ID', 'Trader ID', NULL, 'farrms_admin', GETDATE())
-    PRINT 'Inserted static data value -10000253 - Trader ID.'
+    VALUES (5500, 300006, 'TRM Trader', 'TRM Trader', NULL, 'farrms_admin', GETDATE())
+    PRINT 'Inserted static data value 300006 - TRM Trader.'
 END
 ELSE
 BEGIN
-    PRINT 'Static data value -10000253 - Trader ID already EXISTS.'
+    PRINT 'Static data value 300006 - TRM Trader already EXISTS.'
 END
 SET IDENTITY_INSERT static_data_value OFF
 
-IF NOT EXISTS(SELECT * FROM user_defined_fields_template WHERE field_id = -10000253)
+IF NOT EXISTS(SELECT * FROM user_defined_fields_template WHERE field_id = 300006)
 BEGIN
 	INSERT INTO user_defined_fields_template(field_name, field_label, Field_type, data_type, is_required, sql_string, udf_type, field_size, field_id)
-	SELECT -10000253, 'Trader ID', 'd', 'VARCHAR(100)', 'y', NULL, 'h', 100, -10000253
+	SELECT 300006, 'TRM Trader', 'd', 'VARCHAR(100)', 'y', 'EXEC spa_source_traders_maintain ''x''', 'h', 100, 300006
 END        
 
 IF NOT EXISTS(SELECT 1 FROM generic_mapping_header WHERE mapping_name = 'Prisma Trader Mapping')
@@ -64,25 +64,25 @@ WHERE mapping_name = 'Prisma Trader Mapping'
 
 SELECT @prisma_trader = udf_template_id 
 FROM user_defined_fields_template 
-WHERE Field_id = -10000371
+WHERE Field_id = 50000192
 
 SELECT @trader_id = udf_template_id 
 FROM user_defined_fields_template 
-WHERE Field_id = -10000253
+WHERE Field_id = 300006
 
 IF NOT EXISTS (SELECT 1 FROM generic_mapping_definition WHERE mapping_table_id = @mapping_table_id)
 BEGIN
 	INSERT INTO generic_mapping_definition(
 		mapping_table_id, clm1_label, clm1_udf_id, clm2_label, clm2_udf_id, unique_columns_index
 	)
-	SELECT @mapping_table_id, 'Prisma Trader', @prisma_trader, 'Trader ID', @trader_id, '1'
+	SELECT @mapping_table_id, 'Source Trader', @prisma_trader, 'TRM Trader', @trader_id, '1'
 END
 ELSE
 BEGIN
 	UPDATE gmd
-	SET clm1_label = 'Prisma Trader'
+	SET clm1_label = 'Source Trader'
 	  , clm1_udf_id = @prisma_trader
-	  , clm2_label = 'Prisma ID'
+	  , clm2_label = 'TRM Trader'
 	  , clm2_udf_id = @trader_id
 	FROM generic_mapping_definition gmd
 	INNER JOIN generic_mapping_header gmh 
--- Database/TableChanges/27781_add_generic_mapping_Prisma_Counterparty.sql
+++ Database/TableChanges/27781_add_generic_mapping_Prisma_Counterparty.sql
@@ -1,41 +1,41 @@
-DELETE static_data_value WHERE [type_id] = 5500 AND code = 'Prisma Counterparty' AND value_id > 0
+--DELETE static_data_value WHERE [type_id] = 5500 AND code = 'Prisma Counterparty' AND value_id > 0
 
 SET IDENTITY_INSERT static_data_value ON
-IF NOT EXISTS(SELECT 1 FROM static_data_value WHERE value_id = -10000370)
+IF NOT EXISTS(SELECT 1 FROM static_data_value WHERE Code = 'Source counterparty')
 BEGIN
     INSERT INTO static_data_value ([type_id], [value_id], [code], [description], [category_id], create_user, create_ts)
-    VALUES (5500, -10000370, 'Prisma Counterparty', 'Prisma Counterparty', NULL, 'farrms_admin', GETDATE())
-    PRINT 'Inserted static data value -10000370 - Prisma Counterparty.'
+    VALUES (5500, 50000193, 'Source counterparty', 'Source Counterparty', NULL, 'farrms_admin', GETDATE())
+    PRINT 'Inserted static data value 50000193 - Source Counterparty.'
 END
 ELSE
 BEGIN
-    PRINT 'Static data value -10000370 - Prisma Counterparty already EXISTS.'
+    PRINT 'Static data value -10000370 - Source Counterparty already EXISTS.'
 END
 SET IDENTITY_INSERT static_data_value OFF
 
-IF NOT EXISTS(SELECT 1 FROM user_defined_fields_template WHERE field_id = -10000370)
+IF NOT EXISTS(SELECT 1 FROM user_defined_fields_template WHERE field_id = 50000193)
 BEGIN
 	INSERT INTO user_defined_fields_template(field_name, field_label, Field_type, data_type, is_required, sql_string, udf_type, field_size, field_id)
-	SELECT -10000370, 'Prisma Counterparty', 't', 'VARCHAR(100)', 'y', NULL, 'h', 100, -10000370
+	SELECT 50000193, 'Source Counterparty', 't', 'VARCHAR(100)', 'y', NULL, 'h', 100, 50000193
 END
 
 SET IDENTITY_INSERT static_data_value ON
-IF NOT EXISTS(SELECT 1 FROM static_data_value WHERE value_id = -10000236)
+IF NOT EXISTS(SELECT 1 FROM static_data_value WHERE value_id = 300010)
 BEGIN
     INSERT INTO static_data_value ([type_id], [value_id], [code], [description], [category_id], create_user, create_ts)
-    VALUES (5500, -10000236, 'Counterparty ID', 'Counterparty ID', NULL, 'farrms_admin', GETDATE())
-    PRINT 'Inserted static data value -10000236 - Counterparty ID.'
+    VALUES (5500, 300010, 'TRM Counterparty', 'TRM Counterparty', NULL, 'farrms_admin', GETDATE())
+    PRINT 'Inserted static data value 300010 - TRM Counterparty.'
 END
 ELSE
 BEGIN
-    PRINT 'Static data value -10000236 - Counterparty ID already EXISTS.'
+    PRINT 'Static data value 300010 - Counterparty ID already EXISTS.'
 END
 SET IDENTITY_INSERT static_data_value OFF
 
 IF NOT EXISTS(SELECT 1 FROM user_defined_fields_template WHERE field_id = -10000236)
 BEGIN
 	INSERT INTO user_defined_fields_template(field_name, field_label, Field_type, data_type, is_required, sql_string, udf_type, field_size, field_id)
-	SELECT -10000236, 'Counterparty ID', 'd', 'VARCHAR(100)', 'y', 'SELECT source_counterparty_id, counterparty_id FROM source_counterparty', 'h', 100, -10000236
+	SELECT 300010, 'TRM Counterparty', 'd', 'VARCHAR(100)', 'y', 'EXEC spa_source_counterparty_maintain @flag = ''c'',  @not_int_ext_flag = ''b''', 'h', 100, 300010
 END
 
 IF NOT EXISTS(SELECT 1 FROM generic_mapping_header WHERE mapping_name = 'Prisma Counterparty Mapping')
@@ -64,25 +64,25 @@ WHERE mapping_name = 'Prisma Counterparty Mapping'
 
 SELECT @Prisma_counterparty = udf_template_id 
 FROM user_defined_fields_template 
-WHERE Field_id = -10000370
+WHERE Field_id = 50000193
 
 SELECT @counterparty_id = udf_template_id 
 FROM user_defined_fields_template 
-WHERE Field_id = -10000236
+WHERE Field_id = 300010
 
 IF NOT EXISTS (SELECT 1 FROM generic_mapping_definition WHERE mapping_table_id = @mapping_table_id)
 BEGIN
 	INSERT INTO generic_mapping_definition(
 		mapping_table_id, clm1_label, clm1_udf_id, clm2_label, clm2_udf_id, unique_columns_index
 	)
-	SELECT @mapping_table_id, 'Prisma Counterparty', @Prisma_counterparty, 'Counterparty ID', @counterparty_id, '1'
+	SELECT @mapping_table_id, 'Prisma Counterparty', @Prisma_counterparty, 'TRM Counterparty', @counterparty_id, '1'
 END
 ELSE
 BEGIN
 	UPDATE gmd
-	SET clm1_label = 'Prisma Counterparty'
+	SET clm1_label = 'Source Counterparty'
 	  , clm1_udf_id = @Prisma_counterparty
-	  , clm2_label = 'Counterparty ID'
+	  , clm2_label = 'TRM Counterparty'
 	  , clm2_udf_id = @counterparty_id  
 	  , unique_columns_index = '1'
 	FROM generic_mapping_definition gmd
@@ -178,3 +178,6 @@ BEGIN
 	INSERT INTO generic_mapping_values(mapping_table_id,clm1_value,clm2_value)
 	SELECT @mapping_table_id, 'Nowega GmbH', @counterparty_id_value
 END
+
+
+--Delete from generic_mapping_values where mapping_table_id = 181
