37185fd33261e669b11f1cece98cbad5ca02557b
bijan.karki <bijan.karki>
2021-11-24 05:44:40 +0000

    Fixed shipper default mapped value load issue in copy mode. [Issue: CTRM-4633]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@121531 fd52c265-6bb1-e54c-ad91-b501145a01df

--- FARRMS/trm/adiha.html.forms/_deal_capture/maintain_deals/deal.detail.new.php
+++ FARRMS/trm/adiha.html.forms/_deal_capture/maintain_deals/deal.detail.new.php
@@ -2965,11 +2965,8 @@ $formula_form_data = '[
         var counterparty_id, template_text;
         var contract_id = '';
         var location_id = '';
+        var template_id = '';
 
-        if (deal_id == 'NULL' && copy_deal_id != 'NULL') {
-            deal_id = copy_deal_id;
-        }
-        
         var child_rows = dealDetail.grid.hasChildren(rId) ;
 
         var tab_obj = dealDetail.deal_tab ;   
@@ -2990,6 +2987,7 @@ $formula_form_data = '[
                 var template_obj = form_obj.getCombo('template_id');
                 if (template_obj) {
                     template_text = template_obj.getComboText();
+                    template_id = template_obj.getSelectedValue();
                 }  
             }
                         
