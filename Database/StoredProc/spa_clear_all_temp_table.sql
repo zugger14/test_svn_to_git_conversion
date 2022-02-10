d506e37645729cbcd87257f468ef1b919a043b5a
navaraj.shrestha <navaraj.shrestha>
2022-02-10 11:26:46 +0000

    Reverted the revision : 120640 and 121242 under CTRM-3672 [Issue: CTRM-6592]
    git-svn-id: http://dev.farrms.us:18080/pioneer/products/trmtracker/branches/TRMTracker_Enercity/Ver_4.2.003@123552 fd52c265-6bb1-e54c-ad91-b501145a01df

--- Database/StoredProc/spa_clear_all_temp_table.sql
+++ Database/StoredProc/spa_clear_all_temp_table.sql
@@ -20,17 +20,10 @@ AS
 SET NOCOUNT ON
 
 /*
-EXEC sys.sp_set_session_context @key = N'DB_USER', @value = 'farrms_admin';
+
 declare @nos_of_days INT = NULL,
-	@process_id VARCHAR(150) = 'DEA12BF0_9389_45CE_856B_B7CD8460F6E1',
-	@exclude_tables VARCHAR(max) = 'retain_
-	, alert_
-	, report_position
-	, search_table
-	, step_error_log
-	, missing_deals_pre
-	, TmpEligibleDeals
-	, source_deal_detail_debug' 
+	@process_id VARCHAR(150) = NULL,
+	@exclude_tables VARCHAR(max) = 'report_dataset_cdv,deal_settlement,index_fees_settlement' 
 
 
 if object_id('tempdb..#temp1') is not null drop table #temp1
@@ -60,13 +53,9 @@ CREATE TABLE #temp1(
 --'batch_export_':exclude export tables in deletion action; export table starts with batch_export_
 --'batch_report_power_bi_':exclude power bi tables in deletion action; power bi table starts with batch_report_power_bi_
 
-SET @exclude_tables = isnull(nullif(@exclude_tables,'')+',','') + 'retain_,batch_export_,batch_report_power_bi_'
+set @exclude_tables =isnull(nullif(@exclude_tables,'')+',','')+'batch_export_,batch_report_power_bi_'
 
-SET @exclude_tables = REPLACE(REPLACE(REPLACE(REPLACE(@exclude_tables,CHAR(32),''),CHAR(9),''),CHAR(10),''),CHAR(13),'')
- 
-SELECT LTRIM(RTRIM(tbl.item)) item 
-INTO #except_tables 
-FROM dbo.SplitCommaSeperatedValues(@exclude_tables) tbl
+select tbl.* into #except_tables from dbo.SplitCommaSeperatedValues(@exclude_tables) tbl
 
 IF @process_id IS NULL
 BEGIN
@@ -82,13 +71,14 @@ END
 ELSE
 BEGIN
 	INSERT INTO #temp1 (table_name)
-	SELECT so.[name]
-	FROM dbo.FNASplit(@process_id,',') i 
-	INNER JOIN adiha_process.dbo.sysobjects so WITH(NOLOCK) ON so.[name] LIKE '%' + i.item + '%'
-	LEFT JOIN #except_tables et on so.[name] like et.[item]+'%'
+	SELECT [name]
+	FROM adiha_process.dbo.sysobjects so WITH(NOLOCK)
+		left join #except_tables et on so.[name] like et.[item]+'%'
+
 	WHERE xtype = 'u'
 		AND dbo.FNAGetSQLStandardDate(crdate) <= @sel_date
-		AND et.item is null
+		AND [name] LIKE '%' + @process_id + '%'
+			and et.item is null
 END
 
 
@@ -103,6 +93,6 @@ BEGIN
     WHERE sno = @cnt
 
     EXEC ('DROP TABLE adiha_process.dbo.[' + @tbl_name+']')
-	--print @tbl_name
+	print @tbl_name
     SET @cnt = @cnt + 1
 END
